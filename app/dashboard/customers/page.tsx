69f2b55064101a61d9b31405d514c1ee445d1b1e
Kubabob <j.m.m.bozek@gmail.com>
2025-06-23 16:41:02 +0200

    added simple customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import { Suspense } from 'react';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;    
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<CustomersTableSkeleton />}>
+        <Table customers={await fetchFilteredCustomers(query)} />
+      </Suspense>
+    </div>
+  );
+}
